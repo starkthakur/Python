b4002dcf9d7c8fb5ed82821e172ab1cd273746ae
ashis <ashishthakur.hpu@gmail.com>
2024-01-07 03:32:56 +0530

    updated

--- Lec44_Combine_Img.py
+++ Lec44_Combine_Img.py
@@ -3,8 +3,8 @@ from PIL import Image
 BiggerImg = Image.open("41.jpg")
 SmallerImg = Image.open("hack.png")
 
-# the area of smaller image to superimposed is taken i.e. if small img is 200x200 then area start can be
-# Area start from min 0,0,200,200 or max 1600-200 =1400,900-200=700 to fit on the bigger one
+# //Line 1 : The area of smaller image to be superimposed is taken i.e. if small img is 200x200
+#  //Line 2 : Area start from min 0,0,200,200 or max 1600-200 =1400,900-200=700 to fit on the bigger one
 
 area = (600, 200, 800, 400)
 BiggerImg.paste(SmallerImg, area)
