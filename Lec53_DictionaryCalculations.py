8080b4ef84b7d2b4894f98a90029fca3caf8513d
ashis <ashishthakur.hpu@gmail.com>
2024-01-08 01:28:38 +0530

    updated

---
+++ Lec53_DictionaryCalculations.py
@@ -0,0 +1,20 @@
+stocks = {
+    'Aapl': 201,
+    'IBM': 400,
+    'Google': 700,
+    'Microsoft': 1010,
+    'Tesla': 900,
+    'Amazon': 1200,
+    'HP': 143,
+    'Dell': 192,
+    'Facebook': 421,
+    'Alibaba': 93,
+    'Tata': 245,
+    'zscaler': 231
+}
+print(min(stocks))  # it takes the key insteadof value i.e. the minimum in alphabetical order i.e. Aapl
+print(min(stocks.values()))  # it takes the stock minimum value
+
+# (201 ,aapl) (400, IBM) creating a variable min_price to store data in this format and print
+min_price = min(zip(stocks.values(), stocks.keys()))
+print(min_price)
